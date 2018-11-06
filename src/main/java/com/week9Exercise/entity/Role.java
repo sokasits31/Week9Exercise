8054dfb27a55e8518945d6acf04b997b07cd8919
Steve Sokasits <steve.sokasits@gmail.com>
2018-11-05 22:34:27 -0600

    added DAO stuff with data

---
+++ src/main/java/com/week9Exercise/entity/Role.java
@@ -0,0 +1,87 @@
+package com.week9Exercise.entity;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity(name = "Role")
+@Table(name = "role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id",
+            foreignKey = @ForeignKey(name = "role_user_user_id_fk")
+    )
+    private User user;
+
+    @Column(name="user_name")
+    private String userName;
+
+
+    @Column(name="role_name")
+    private String roleName;
+
+    public Role() {
+    }
+
+    public Role(User user, String roleName, String userName) {
+        this.user = user;
+        this.roleName = roleName;
+        this.userName = userName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id=" + id +
+                ", roleName='" + roleName + '\'' +
+                ", userName= '" + userName + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role = (Role) o;
+        return id == role.id &&
+                Objects.equals(userName, role.userName) &&
+                Objects.equals(roleName, role.roleName);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id,  roleName, userName);
+    }
+}
+
